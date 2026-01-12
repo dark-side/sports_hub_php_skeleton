54e5b7dbb232ba723feaaa576928771bd14381f0
Ostap Matsko <30385865+Ostaps@users.noreply.github.com>
2026-01-12 17:56:23 +0200

    Add user registration route with AuthController

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/users/registrations', [AuthController::class, 'signup'])->name('user.create');
